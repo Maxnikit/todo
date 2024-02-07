34d5c9969dc670e17ddc52e5136bc2a0cc4ef99d
Maxnikit <max.nikit.03@gmail.com>
2024-02-08 02:10:25 +0300

    clean up index

--- src/index.js
+++ src/index.js
@@ -6,32 +6,14 @@ import TodoList from "./todoList.js";
 import Project from "./project.js";
 import Dom from "./dom.js";
 import Storage from "./storage.js";
-
 import dateTest from "./date.js";
+
 // Storage.clearStorage();
 Storage.getAndRefreshTodoList();
-let task1 = new Task("Exam", "Prepare for exams", "2022-01-01", "high");
-let task2 = new Task("Make a website", "JS+CSS", "2022-01-02", "low");
-let task3 = new Task("Study", "Study for 1 hour", "2022-01-03", "medium");
-let task4 = new Task("Chill", "Relax", "2022-01-04", "high");
-// Storage.addProject(new Project("Study"));
-// let workProject = new Project("Work", "orange");
-// todoList.addProject(workProject);
-// let otherProject = new Project("Other", "green");
-// todoList.addProject(otherProject);
 
-// Project.addTask(studyProject, task1);
-// Project.addTask(workProject, task2);
-// Project.addTask(studyProject, task3);
-// Project.addTask(otherProject, task4);
 Dom.loadPage();
 const Inbox = Storage.getAndRefreshTodoList().getProject("Inbox");
 const Upcoming = Storage.getAndRefreshTodoList().getProject("Upcoming");
-// Upcoming.addTask(task1);
-
-// Upcoming.addTask(task2);
-// Upcoming.addTask(task3);
-// TODO When deleting project it shows its tasks
 
 const testDates = document.querySelectorAll(".calendar");
 console.log(testDates);
